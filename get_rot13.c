bdd420e0d1bf39b6cedc63731cddd65849d7cbed
MohamedElkaffas <mohamedalkaffas2000@gmail.com>
2023-04-25 17:54:09 -0700

    at

--- get_rot13.c
+++ get_rot13.c
@@ -2,33 +2,33 @@
 
 /**
  * _rot13 - prints rot13 version
- * @r list being passed
+ * @rot: list being passed
  * Return: count of chars
  */
-int _rot13(va_list r)
+int _rot13(va_list rot)
 {
-    int a, cou;
-    char *s;
+	int a, cnt;
+	char *s;
 
-    a = cou = 0;
-    s = va_arg(r, char *);
+	a = cnt = 0;
+	s = va_arg(rot, char *);
 
-    if (s == NULL)
-        return (-1);
-    while (s[a] != '\0')
-    {
-        if ((s[a] >= 'a' && s[a] <= 'z') ||
-            (s[a] >= 'A' && s[a] <= 'Z'))
-        {
-            if ((s[a] >= 'n' && s[a] <= 'z') ||
-                (s[a] >= 'N' && s[a] <= 'Z'))
-                cou = cou + _putchar(s[a] - 13);
-            else
-                cou = cou + _putchar(s[a] + 13);
-        }
-        else
-            cou = cou + _putchar(s[a]);
-        a++;
-    }
-    return (cou);
+	if (s == NULL)
+		return (-1);
+	while (s[a] != '\0')
+	{
+		if ((s[a] >= 'a' && s[a] <= 'z') ||
+		    (s[a] >= 'A' && s[a] <= 'Z'))
+		{
+			if ((s[a] >= 'n' && s[a] <= 'z') ||
+			    (s[a] >= 'N' && s[a] <= 'Z'))
+				cnt += _putchar(s[a] - 13);
+			else
+				cnt += _putchar(s[a] + 13);
+		}
+		else
+			cnt += _putchar(s[a]);
+		++a;
+	}
+	return (cnt);
 }
